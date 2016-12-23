82d85b9b7225401b1e30eadd348e8962f7c7a0e4
Martin Wheatman <martin@wheatman.net>
2016-12-23 16:54:30 +0000

    Tidy - Fmt, won't work with short option, but working on it...

--- com/yagadi/enguage/expression/Fmt.java
+++ com/yagadi/enguage/expression/Fmt.java
@@ -19,21 +19,20 @@ public class Fmt {
 	public  boolean variable() {return v;}
 	
 	private Strings ormat = new Strings();
-	/*private Strings format() {
-		if (!verbose()) {
-			if (f.ormat().size() > 1 && f.ormat().get( 1 ).equals( "," ))
-				if (f.ormat().get( 0 ).equalsIgnoreCase( yes ) || // yes is set to "OK", when yes is "yes", this fails...
-					f.ormat().get( 0 ).equalsIgnoreCase(  no ) ||
-					f.ormat().get( 0 ).equalsIgnoreCase( success )) {
-					return new Strings( say() + " " +f.ormat().get( 0 ));
-				} else if (f.ormat().get( 0 ).equalsIgnoreCase( failure )) {
-					return new Strings( say()).append( f.ormat().copyAfter( 1 ).filter()); // forget 1st & 2nd
+	public  Strings ormat() {
+		/*if (shrt) {
+			if (ormat().size() > 1 && ormat().get( 1 ).equals( "," ))
+				if (ormat().get( 0 ).equalsIgnoreCase( yes ) || // yes is set to "OK", when yes is "yes", this fails...
+					ormat().get( 0 ).equalsIgnoreCase(  no ) ||
+					ormat().get( 0 ).equalsIgnoreCase( success )) {
+					return ormat().get( 0 ));
+				} else if (ormat().get( 0 ).equalsIgnoreCase( failure )) {
+					return ormat().copyAfter( 1 ).filter(); // forget 1st & 2nd
 				}
-			return new Strings( say()).append( f.ormat().filter( ));
-		}
-		return f.ormat( );
-	}*/
-	public  Strings ormat() {return ormat;}
+			return ormat().filter();
+		}*/
+		return ormat;
+	}
 	public  void    ormat(String s) {
 		ormat = Colloquial.applyOutgoing(
 					Context.deref(
