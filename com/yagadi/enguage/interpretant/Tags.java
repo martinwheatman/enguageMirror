3eba724196f316bd57573d1701bc7076b837a75b
Martin Wheatman <martin@wheatman.net>
2017-04-19 12:27:23 +0100

    Tidy - Tags -> ctor, members/methods, to strings, test code

--- com/yagadi/enguage/interpretant/Tags.java
+++ com/yagadi/enguage/interpretant/Tags.java
@@ -17,74 +17,8 @@ public class Tags extends ArrayList<Tag> {
 	static final         long serialVersionUID = 0;
 	static private       Audit           audit = new Audit( "Tags", false );
 	
-	static private       boolean debug = false;
-	static public        boolean debug() { return debug; }
-	static public        void    debug( boolean b ) { debug = b; }
-	
-	
-	// with postfix boilerplate:
-	// typically { [ ">>>", "name1" ], [ "/", "name2" ], [ "/", "name3" ], [ "<<<", "" ] }.
-	// could be  { [ ">>>", "name1", "" ], [ "/", "name2", "" ], [ "/", "name3", "<<<" ] }.
-	public String toXml() { return toXml( 0 );}
-	public String toXml( int level ) {
-		String str="";
-		Iterator<Tag> ti = iterator();
-		while (ti.hasNext()) {
-			str += ti.next().toXml( level );
-			if (ti.hasNext()) str += " ";
-		}
-		return str;
-	}
-	public String toString() {
-		String str="";
-		Iterator<Tag> ti = iterator();
-		while (ti.hasNext()) {
-			str += ti.next().toString();
-			if (ti.hasNext()) str += " ";
-		}
-		return str;
-	}
-	public String toText() {
-		String str="";
-		Iterator<Tag> ti = iterator();
-		while (ti.hasNext()) {
-			str += ti.next().toText();
-			if (ti.hasNext()) str += " ";
-		}
-		return str;
-	}
-	public String toLine() {
-		String str="";
-		Iterator<Tag> ti = iterator();
-		while (ti.hasNext()) {
-			Tag t = ti.next();
-			str += ( " "+t.prefixAsStrings().toString()+" <"+t.name() +" "+ t.attributes().toString() +"/> "+t.postfix());
-		}
-		return str;
-	}
-
 	public Tags() { super(); }
-	public boolean equals( Tags ta ) {
-		if (ta == null || size() != ta.size())
-			return false;
-		else {
-			Iterator<Tag> it = iterator(), tait = ta.iterator();
-			while (it.hasNext())
-				if (!it.next().equals( tait.next() ))
-					return false;
-		}
-		return true;
-	}
-	public boolean matches( Tags patterns ) {
-		if (patterns.size() == 0) return true; // ALL = "" 
-		if (patterns == null || size() < patterns.size()) return false;
-		Iterator<Tag> it = iterator(),
-				pit = patterns.iterator();
-		while (it.hasNext()) // ordered by patterns
-			if (!it.next().matches( pit.next() ))
-				return false;
-		return true;
-	}
+	
 	// "if X do Y" -> [ <x pref="if "/>, <y pref="do "/>, <pref="."> ]
 	public Tags( String str ) {
 		Strings words = new Strings( str ); // correct? ...FromChars? ...NonWS?
@@ -183,6 +117,33 @@ public class Tags extends ArrayList<Tag> {
 		}
 		if (!t.isNull()) add( t.prefixAsStrings( prefix ));
 	}
+	
+	static private       boolean debug = false;
+	static public        boolean debug() { return debug; }
+	static public        void    debug( boolean b ) { debug = b; }
+	
+	public boolean equals( Tags ta ) {
+		if (ta == null || size() != ta.size())
+			return false;
+		else {
+			Iterator<Tag> it = iterator(), tait = ta.iterator();
+			while (it.hasNext())
+				if (!it.next().equals( tait.next() ))
+					return false;
+		}
+		return true;
+	}
+	public boolean matches( Tags patterns ) {
+		if (patterns.size() == 0) return true; // ALL = "" 
+		if (patterns == null || size() < patterns.size()) return false;
+		Iterator<Tag> it = iterator(),
+				pit = patterns.iterator();
+		while (it.hasNext()) // ordered by patterns
+			if (!it.next().matches( pit.next() ))
+				return false;
+		return true;
+	}
+
 	/* *************************************************************************
 	 * matchValues() coming soon...
 	 */
@@ -322,6 +283,46 @@ public class Tags extends ArrayList<Tag> {
 		
 		//if (debug) audit.traceOut( "matched => "+ (matched==null ? "no values" : matched.toString()));
 		return null == matched ? new Attributes() : matched;
+	}
+	// with postfix boilerplate:
+	// typically { [ ">>>", "name1" ], [ "/", "name2" ], [ "/", "name3" ], [ "<<<", "" ] }.
+	// could be  { [ ">>>", "name1", "" ], [ "/", "name2", "" ], [ "/", "name3", "<<<" ] }.
+	public String toXml() { return toXml( 0 );}
+	public String toXml( int level ) {
+		String str="";
+		Iterator<Tag> ti = iterator();
+		while (ti.hasNext()) {
+			str += ti.next().toXml( level );
+			if (ti.hasNext()) str += " ";
+		}
+		return str;
+	}
+	public String toString() {
+		String str="";
+		Iterator<Tag> ti = iterator();
+		while (ti.hasNext()) {
+			str += ti.next().toString();
+			if (ti.hasNext()) str += " ";
+		}
+		return str;
+	}
+	public String toText() {
+		String str="";
+		Iterator<Tag> ti = iterator();
+		while (ti.hasNext()) {
+			str += ti.next().toText();
+			if (ti.hasNext()) str += " ";
+		}
+		return str;
+	}
+	public String toLine() {
+		String str="";
+		Iterator<Tag> ti = iterator();
+		while (ti.hasNext()) {
+			Tag t = ti.next();
+			str += ( " "+t.prefixAsStrings().toString()+" <"+t.name() +" "+ t.attributes().toString() +"/> "+t.postfix());
+		}
+		return str;
 	}
 
 	
