e4a4b0b442d0956bc730ef2343b3183d0ba3cac0
Martin Wheatman <martin@wheatman.net>
2017-04-26 14:21:19 +0100

    Fix: variable => variable variable

--- com/yagadi/enguage/Enguage.java
+++ com/yagadi/enguage/Enguage.java
@@ -284,8 +284,8 @@ public class Enguage extends Shell {
 			audit.title( "On-the-fly Langauge Learning" );
 			testInterpret( "my name is martin", "I don't understand" );
 			testInterpret( "interpret my name is phrase variable name thus", "go on." );
-			testInterpret( "first perform variable set name martin", "go on." );
-			testInterpret( "then perform variable get name", "go on." );
+			testInterpret( "first perform variable variable set name martin", "go on." );
+			testInterpret( "then perform variable variable get name", "go on." );
 			testInterpret( "then reply hello whatever", "go on." );
 			testInterpret( "that is it", "ok." );
 			//Repertoire.signs.show();
