435aab5eae49f2e745cb8f12d0f4b465fdc344be
Martin Wheatman <martin@wheatman.net>
2017-10-06 21:19:35 +0100

    Add: cosmetic changes for SGAI

--- com/yagadi/enguage/Enguage.java
+++ com/yagadi/enguage/Enguage.java
@@ -151,36 +151,36 @@ public class Enguage extends Shell {
 			testInterpret( "i don't need anything", null );
 
 			testInterpret( "what do i need",
-						   "you don't need anything." );
+						   "you don't need anything" );
 			testInterpret( "i need 2 cups of coffee and a biscuit",
 						   "ok, you need 2 cups of coffee, and a biscuit.");
 			testInterpret( "what do i need",
 						   "you need 2 cups of coffee, and a biscuit.");
 			testInterpret( "how many coffees do i need",
-						   "2, you need 2 coffees." );
+						   "2, you need 2 coffees" );
 			testInterpret( "i don't need any coffee",
-						   "ok, you don't need any coffee." );
+						   "ok, you don't need any coffee" );
 			testInterpret( "what do i need",
-						   "you need a biscuit." );
+						   "you need a biscuit" );
 
 			audit.title( "Semantic Thrust" );
 			testInterpret( "i need to go to town",
-						   "ok, you need to go to town." );
+						   "ok, you need to go to town" );
 			testInterpret( "what do i need",
-						   "you need a biscuit, and to go to town." );
+						   "you need a biscuit, and to go to town" );
 			testInterpret( "i have a biscuit",
-						   "ok, you don't need a biscuit." );
+						   "ok, you don't need a biscuit" );
 			testInterpret( "i have to go to town",
-						   "I know." );
+						   "I know" );
 			testInterpret( "i don't need to go to town",
-						   "ok, you don't need to go to town." );
+						   "ok, you don't need to go to town" );
 			
 			Question.primedAnswer( "yes" );
 			testInterpret( "I have everything",
-					       "ok, you don't need anything." );
+					       "ok, you don't need anything" );
 			
 			testInterpret( "what do i need",
-						   "you don't need anything." );
+						   "you don't need anything" );
 		}
 		if ( level == 0 || level == 3 ) {
 			audit.title( "Verbal Arithmetical" );
@@ -189,54 +189,56 @@ public class Enguage extends Shell {
 			testInterpret( "times 2 all squared",
 						   "times 2 all squared makes 36.");
 			testInterpret( "what is 36 + 4 all divided by 2",
-						   "36 plus 4 all divided by 2 is 20." );
+						   "36 plus 4 all divided by 2 is 20" );
 			
+			audit.title( "Factorial Description" );
+			testInterpret( "what is the factorial of 4", "I don't understand" );
 			/* Ideally, we want:
 			 * - the factorial of 1 is 1;
 			 * - the factorial of n is n times the factorial of n - 1;
 			 * - what is the factorial of 3.
 			 */
-			testInterpret( "to the phrase what is the factorial of 1 reply 1", "go on." );
-			testInterpret( "ok", "ok." );
-			testInterpret( "what is the factorial of 1",  "1." );
+			testInterpret( "to the phrase what is the factorial of 1 reply 1", "go on" );
+			testInterpret( "ok", "ok" );
+			testInterpret( "what is the factorial of 1",  "1" );
 			
-			testInterpret( "interpret multiply numeric phrase variable a by numeric phrase variable b thus", "go on." );
-			testInterpret( "first perform numeric evaluate variable a times numeric phrase variable b",      "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "interpret multiply numeric variable a by numeric variable b thus", "go on" );
+			testInterpret( "first perform numeric evaluate variable a times numeric phrase variable b",      "go on" );
+			testInterpret( "ok", "ok" );
 			
-			testInterpret( "multiply 2 by 3", "6." );
+			testInterpret( "multiply 2 by 3", "6" );
 			
-			testInterpret( "interpret subtract numeric phrase variable c from numeric phrase variable d thus", "go on." );
-			testInterpret( "first perform numeric evaluate variable d - variable c",                           "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "interpret subtract numeric variable c from numeric variable d thus", "go on" );
+			testInterpret( "first perform numeric evaluate variable d - variable c",                           "go on" );
+			testInterpret( "ok", "ok" );
 			
-			testInterpret( "subtract 2 from 3", "1." );
+			testInterpret( "subtract 2 from 3", "1" );
 			
-			testInterpret( "interpret what is the factorial of numeric phrase variable n thus", "go on." );
-			testInterpret( "first subtract 1 from variable n",                                  "go on." );
-			testInterpret( "then what is the factorial of whatever",                            "go on." );
-			testInterpret( "then multiply whatever by variable n",                              "go on." );
-			testInterpret( "then reply whatever the factorial of variable n is whatever",       "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "interpret what is the factorial of numeric variable n thus",  "go on" );
+			testInterpret( "first subtract 1 from variable n",                            "go on" );
+			testInterpret( "then what is the factorial of whatever",                      "go on" );
+			testInterpret( "then multiply whatever by variable n",                        "go on" );
+			testInterpret( "then reply whatever the factorial of variable n is whatever", "go on" );
+			testInterpret( "ok", "ok" );
 			
 			//Repertoire.signs.show( "OTF" );
 			//testInterpret("tracing on");
-			testInterpret( "what is the factorial of 4",  "24 the factorial of 4 is 24." );
+			testInterpret( "what is the factorial of 4",  "24 the factorial of 4 is 24" );
 		}
 		if ( level == 0 || level == 4 ) {
 			audit.title( "Numerical Context" );
 			testInterpret( "i need a coffee",
-						   "ok, you need a coffee." );
+						   "ok, you need a coffee" );
 			testInterpret( "and another",
-						   "ok, you need 1 more coffee." );
+						   "ok, you need 1 more coffee" );
 			testInterpret( "how many coffees do i need",
-						   "2, you need 2 coffees." );
+						   "2, you need 2 coffees" );
 			testInterpret( "i need a cup of tea",
-						   "ok, you need a cup of tea." );
+						   "ok, you need a cup of tea" );
 			testInterpret( "and another coffee",
-						   "ok, you need 1 more coffee." );
+						   "ok, you need 1 more coffee" );
 			testInterpret( "what do i need",
-						   "You need 3 coffees , and a cup of tea." );
+						   "You need 3 coffees , and a cup of tea" );
 		}
 		if ( level == 0 || level == 5 ) {
 			audit.title( "Correction" );
@@ -248,7 +250,7 @@ public class Enguage extends Shell {
 						   "you need 6 coffees, and a cup of tea.");
 			Question.primedAnswer( "yes" );
 			testInterpret( "i don't need anything",
-						   "ok, you don't need anything." );
+						   "ok, you don't need anything" );
 		}
 		if ( level == 0 || level == 6 ) {
 			audit.title( "Disambiguation" );
@@ -276,21 +278,21 @@ public class Enguage extends Shell {
 			
 			//Where.locatorIs( "at" ); is the same as...
 			// new Sofa().interpret( new Strings( "spatial locator at" )); is the same as...
-			testInterpret( "interpret spatially something can be phrase variable locator thus", "go on." );
-			testInterpret( "first perform spatial locator variable locator", "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "interpret spatially something can be phrase variable locator thus", "go on" );
+			testInterpret( "first perform spatial locator variable locator", "go on" );
+			testInterpret( "ok", "ok" );
 			
-			testInterpret( "spatially something can be to the left of",  "ok." );
-			testInterpret( "spatially something can be to the right of", "ok." );
-			testInterpret( "spatially something can be in front of",     "ok." );
-			testInterpret( "spatially something can be on top of",       "ok." );
-			testInterpret( "spatially something can be behind",          "ok." );
-			testInterpret( "spatially something can be in",              "ok." );
-			testInterpret( "spatially something can be on",              "ok." );
-			testInterpret( "spatially something can be under",           "ok." );
-			testInterpret( "spatially something can be underneath",      "ok." );
-			testInterpret( "spatially something can be over",            "ok." );
-			testInterpret( "spatially something can be at",              "ok." );
+			testInterpret( "spatially something can be to the left of",  "ok" );
+			testInterpret( "spatially something can be to the right of", "ok" );
+			testInterpret( "spatially something can be in front of",     "ok" );
+			testInterpret( "spatially something can be on top of",       "ok" );
+			testInterpret( "spatially something can be behind",          "ok" );
+			testInterpret( "spatially something can be in",              "ok" );
+			testInterpret( "spatially something can be on",              "ok" );
+			testInterpret( "spatially something can be under",           "ok" );
+			testInterpret( "spatially something can be underneath",      "ok" );
+			testInterpret( "spatially something can be over",            "ok" );
+			testInterpret( "spatially something can be at",              "ok" );
 			
 			/* TODO: interpret think of a variable entity thus.  // see sofa for particular details!
 			 * first create a class variable entity.             // mkdir pub; touch pub/isa 
@@ -303,26 +305,26 @@ public class Enguage extends Shell {
 			//new Sofa().interpret( new Strings( "entity create pub" ));
 
 			testInterpret( "I'm not meeting anybody",
-					"Ok , you're not meeting anybody." );
+					"Ok , you're not meeting anybody" );
 			testInterpret( "At 7 I'm meeting my brother at the pub",
-					"Ok , you're meeting your brother at 7 at the pub." );
+					"Ok , you're meeting your brother at 7 at the pub" );
 			testInterpret( "When  am I meeting my brother",
-					"You're meeting your brother at 7." );
+					"You're meeting your brother at 7" );
 			testInterpret( "Where am I meeting my brother",
-					"You're meeting your brother at the pub." );
+					"You're meeting your brother at the pub" );
 			testInterpret( "Am I meeting my brother",
-					"Yes , you're meeting your brother." );
+					"Yes , you're meeting your brother" );
 			
 			//testInterpret( "tracing on" );
 			testInterpret( "I'm meeting my sister at the pub" );
 			testInterpret( "When am I meeting my sister",
-					"I don't know when you're meeting your sister." );
+					"I don't know when you're meeting your sister" );
 			
 			//testInterpret( "tracing on" );
 			testInterpret( "When am I meeting my dad",
-					"i don't know if you're meeting your dad." );
+					"i don't know if you're meeting your dad" );
 			testInterpret( "Where am I meeting my dad" ,
-					"i don't know if you're meeting your dad." );
+					"i don't know if you're meeting your dad" );
 			
 			/* TODO:
 			 *  create a queen called elizabeth the first  (eliz = woman's name, a queen is a monarch => person)
@@ -331,8 +333,8 @@ public class Enguage extends Shell {
 			 */
 		}
 		if (level == 0 || level == 8) {
-			testInterpret( "tcpip localhost 999 \"999 is a test value for port address\"",   "ok." );
-			testInterpret( "tcpip localhost 5678 \"this is a test, which will fail\"",    "Sorry." );
+			testInterpret( "tcpip localhost 999 \"999 is a test value for port address\"",   "ok" );
+			testInterpret( "tcpip localhost 5678 \"this is a test, which will fail\"",    "Sorry" );
 		}
 		if (level == 0 || level == 9) {
 			audit.title( "On-the-fly Langauge Learning" );
@@ -344,56 +346,56 @@ public class Enguage extends Shell {
 			 */
 			
 			// First, what we can't say yet...
-			testInterpret( "my name is martin",                 "I don't understand." );
-			testInterpret( "if not  reply i already know this", "I don't understand." );
-			testInterpret( "unset the value of name",           "ok." );
+			testInterpret( "my name is martin",                 "I don't understand" );
+			testInterpret( "if not  reply i already know this", "I don't understand" );
+			testInterpret( "unset the value of name",           "ok" );
 			
 			// build-a-program...
-			testInterpret( "interpret my name is phrase variable name thus", "go on." );
-			testInterpret( "first set name to variable name",                "go on." );
-			testInterpret( "then get the value of name",                     "go on." ); // not strictly necessary!
-			testInterpret( "then reply hello whatever",                      "go on." );
+			testInterpret( "interpret my name is phrase variable name thus", "go on" );
+			testInterpret( "first set name to variable name",                "go on" );
+			testInterpret( "then get the value of name",                     "go on" ); // not strictly necessary!
+			testInterpret( "then reply hello whatever",                      "go on" );
 			testInterpret( "and that is it",                                 "ok."    );
 
-			testInterpret( "my name is ruth",    "hello ruth." );
-			testInterpret( "my name is martin",  "hello martin." );
+			testInterpret( "my name is ruth",    "hello ruth" );
+			testInterpret( "my name is martin",  "hello martin" );
 			
 			//...or to put it another way
-			testInterpret( "to the phrase i am called phrase variable name reply hi whatever", "go on." );
-			testInterpret( "this implies name gets set to variable name", "go on." );
-			testInterpret( "this implies name is not set to variable name", "go on." );
-			testInterpret( "if not reply i already know this", "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "to the phrase i am called phrase variable name reply hi whatever", "go on" );
+			testInterpret( "this implies name gets set to variable name", "go on" );
+			testInterpret( "this implies name is not set to variable name", "go on" );
+			testInterpret( "if not reply i already know this", "go on" );
+			testInterpret( "ok", "ok" );
 			
-			testInterpret( "i am called martin", "i already know this." );
+			testInterpret( "i am called martin", "i already know this" );
 			
 			// ...means.../...the means to...
 			// 1. from the-means-to repertoire
-			testInterpret( "to the phrase phrase variable x the means to phrase variable y reply i really do not understand", "go on." );
-			testInterpret( "that is it", "ok." );
+			testInterpret( "to the phrase phrase variable x the means to phrase variable y reply i really do not understand", "go on" );
+			testInterpret( "that is it", "ok" );
 
-			testInterpret( "do we have the means to become rich", "I really don't understand." );
+			testInterpret( "do we have the means to become rich", "I really don't understand" );
 
 			// 2. could this be built thus?
-			testInterpret( "to phrase variable this means phrase variable that reply ok", "go on." );
-			testInterpret( "this implies perform sign think variable that",               "go on." );
-			testInterpret( "this implies perform sign create variable this",              "go on." );
-			testInterpret( "ok", "ok." );
+			testInterpret( "to phrase variable this means phrase variable that reply ok", "go on" );
+			testInterpret( "this implies perform sign think variable that",               "go on" );
+			testInterpret( "this implies perform sign create variable this",              "go on" );
+			testInterpret( "ok", "ok" );
 			
-			testInterpret( "just call me phrase variable name means i am called variable name", "ok." );
-			testInterpret( "just call me martin", "i already know this." );
+			testInterpret( "just call me phrase variable name means i am called variable name", "ok" );
+			testInterpret( "just call me martin", "i already know this" );
 		}
 		if ( level == 0 || level == 10 ) {
 			audit.title( "Ask: Confirmation" );
 			
 			Question.primedAnswer( "yes" );
-			testInterpret( "i have everything", "ok , you don't need anything." );
+			testInterpret( "i have everything", "ok , you don't need anything" );
 			
 			Question.primedAnswer( "no" );
-			testInterpret( "i have everything", "ok , let us leave things as they are." );
+			testInterpret( "i have everything", "ok , let us leave things as they are" );
 
 			Question.primedAnswer( "i do not understand" );
-			testInterpret( "i have everything", "Ok , let us leave things as they are." );
+			testInterpret( "i have everything", "Ok , let us leave things as they are" );
 			
 			/* TODO:
 			 * To the phrase: i am p v name       => set user name NAME
