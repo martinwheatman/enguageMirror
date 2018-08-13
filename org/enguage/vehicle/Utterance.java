413266710419a1ac0b6916d7f3ff24867745583e
Martin Wheatman <9132000+martinwheatman@users.noreply.github.com>
2018-08-13 13:17:19 +0100

    Perf - only check for when if temporal. Plus tidy.

--- org/enguage/vehicle/Utterance.java
+++ org/enguage/vehicle/Utterance.java
@@ -1,5 +1,6 @@
 package org.enguage.vehicle;
 
+import org.enguage.Enguage;
 import org.enguage.interp.Context;
 import org.enguage.interp.pattern.Pattern;
 import org.enguage.interp.pattern.Patternette;
@@ -8,14 +9,12 @@ import org.enguage.objects.Numeric;
 import org.enguage.objects.Spatial;
 import org.enguage.objects.Temporal;
 import org.enguage.objects.Variable;
+import org.enguage.objects.space.Overlay;
 import org.enguage.objects.space.Sofa;
 import org.enguage.util.Audit;
 import org.enguage.util.Strings;
 import org.enguage.util.attr.Attributes;
 import org.enguage.util.sys.Shell;
-import org.enguage.vehicle.Colloquial;
-import org.enguage.vehicle.Language;
-import org.enguage.vehicle.Utterance;
 import org.enguage.vehicle.reply.Answer;
 import org.enguage.vehicle.when.When;
 import org.enguage.vehicle.where.Where;
@@ -33,7 +32,7 @@ public class Utterance {
 	private Strings expanded;
 	public  Strings expanded() { return expanded; }
 	private When    when;
-	private Attributes a;
+	private Attributes whenAttrs;
 	// ... extensible?
 
 	/* Utterance stores all the versions of what has been said, recognising snippets:
@@ -43,39 +42,32 @@ public class Utterance {
 	 */
 	public Utterance( Strings orig ) { this( orig, null ); }
 	public Utterance( Strings orig, Strings prevAnswer ) {
-		//audit.in( "ctor", orig.toString( Strings.SPACED ));
 		representamen = orig;
 		expanded =	Colloquial.applyIncoming(       // I'm => I am => _user is
 						new Strings( orig ).normalise() // "i am/." becomes "i/am/."
 					).decap()                          // deref anything in the environment?
 					.contract( "=" );                  // [ name, =, "value" ] => [ name="value" ]
-		//audit.LOG( "Utterance: expanded: "+ expanded.toString( Strings.SPACED ));
 
-		if (null != prevAnswer && expanded.contains( Answer.placeholder())) {
-			//audit.LOG( ">>>>>replacing "+ Ans.placeholderAsStrings() +" with "+ new Strings( prevAnswer ));
+		// insert answer, e.g. "abc whatever xyz" + prevAnswer='42' => "abc 42 xyz"
+		if (null != prevAnswer && expanded.contains( Answer.placeholder()))
 			expanded.replace( Answer.placeholderAsStrings(), new Strings( prevAnswer ));
-		}
 			
-		temporal = new Strings( expanded );
-		when = When.getWhen( temporal );
-		
-		a = when.toAttributes();
-		//audit.out( "when: "+ when +", where: "+ where +", t/s: "+ whenWhere );
+		temporal  = new Strings( expanded );
+		when      = When.getWhen( temporal ); // removes temporal content
+		whenAttrs = when.toAttributes();
 	}
 	
 	public Attributes match( Sign s ) {
 		// This is a high value method so could do with being fast!
 		Attributes match = null;
 
-		Context.push( a );
-		
-		if (s.isTemporal()
-			&& !when.isUnassigned()
-			&& (null != (match = s.pattern().matchValues( temporal, s.isSpatial() ))))
-			match.addAll( a ); // add it, don't not pop it.
-
-		Context.pop();  // do pop it! Keep context clean!
-		
+		if (s.isTemporal()) {
+			Context.push( whenAttrs );
+			if (!when.isUnassigned() &&
+			    (null != (match = s.pattern().matchValues( temporal, s.isSpatial() ))))
+				match.addAll( whenAttrs ); // add it, don't not pop it.
+			Context.pop();  // do pop it! Keep context clean!
+		}
 		// if no qualified match, attempt an expanded match
 		return match == null ? s.pattern().matchValues( expanded, s.isSpatial() ) : match;
 	}
@@ -120,6 +112,11 @@ public class Utterance {
 	
 	public static void main( String arg[]) {
 		
+		Enguage.e = new Enguage();
+		if ((null == Enguage.e.o || !Enguage.e.o.attached())
+			&& !Overlay.autoAttach())
+			audit.FATAL(">>>>Ouch! Cannot autoAttach() to object space<<<<" );
+
 		Where.doLocators( "to the left of/to the right of/in front of/on top of");
 		Where.doLocators( "behind/in/on/under/underneath/over/at" );
 		
