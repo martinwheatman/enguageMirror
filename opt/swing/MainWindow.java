55a747a03e39ea1e414194e7bfdfb00b30594163
Megan Challenger <meganchallenger@hotmail.com>
2021-05-13 01:34:17 +0100

    Log in panel and main window created

--- opt/swing/MainWindow.java
+++ opt/swing/MainWindow.java
@@ -1,28 +1,91 @@
 package opt.swing;
 
+
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.media.*;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MainWindow {
-	
-	public class mediaPlayer extends JFrame
-    {
-        public mediaPlayer()
-        {
-            setLayout(new BorderLayout());
+import opt.swing.LogInPanel.ButtonAction;
 
-            //file you want to play
-            URL mediaURL = Player mediaPlayer = Manager.createRealizedPlayer(mediaURL);
-            //get components for video and playback controls
-            Component video = mediaPlayer.getVisualComponent();
-            Component controls = mediaPlayer.getControlPanelComponent();
-            add(video,BorderLayout.CENTER);
-            add(controls,BorderLayout.SOUTH);
-        }
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame implements ActionListener {
+	 Container container = getContentPane();
+	
+	MainWindow() throws NoPlayerException, CannotRealizeException, IOException {
+        setLayoutManager();
     }
+	  public void setLayoutManager() throws NoPlayerException, CannotRealizeException, IOException {
+	        container.setLayout(null);
+	    //    load();
+	    }
+	 
+	 
+//	public static void main(String[] a) throws NoPlayerException, CannotRealizeException, IOException {
+//	    
+//		MainWindow frame = new MainWindow();
+//	    frame.setTitle("Login Window");
+//	    frame.setVisible(true);
+//	    frame.setBounds(10, 10, 370, 600);
+//	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	    frame.setResizable(false);
+//	    
+//	}
+       
+
+
+		public void load() throws NoPlayerException, CannotRealizeException, IOException
+        {
+            
+
+            String url = "https://www.youtube.com/watch?v=rXAkLKcxxM8";
+            URL mediaURL;
+			try {
+			//	mediaURL = new URL("https://www.youtube.com/watch?v=rXAkLKcxxM8");
+		
+          //  Player mediaPlayer = Manager.createRealizedPlayer(mediaURL);
+           // Component video = mediaPlayer.getVisualComponent();
+          //  Component controls = mediaPlayer.getControlPanelComponent();
+            
+            MainWindow 
+    		
+    			frame = new MainWindow();
+    			 frame.setTitle("Main Window");
+    			    frame.setVisible(true);
+    			    frame.setBounds(200, 50, 1000, 600);
+    			    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    			    frame.setResizable(false);
+    			//    container.add(video,BorderLayout.CENTER);
+    	          //  container.add(controls,BorderLayout.SOUTH);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+         
+           
+        }
+
+
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+    
 
 
 }
